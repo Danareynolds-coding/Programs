962fc04a9dd1694ef4478b60ae2cd3b6d3744e6b
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-11-25 20:12:27 -0600

    fixed error in dao common

--- daos/common/daoCommon.js
+++ daos/common/daoCommon.js
@@ -1,29 +1,29 @@
 // dao  data access object
 // daoCommon holds methods that all other tables can use//
-const connect = require('../config/dbconfig')
+const connect = require('../../config/dbconfig')
 //querry database methods 2args 1.sqlquery 2.callback function 
 const daoCommon = {
-      findAll:(req, res, table)=> {
-      connect.query(
-        `SELECT * FROM ${table};`,        //sql query
-        (error, rows)=> {                 //callback funtion
-          if(!error) {
-            if(rows.length === 1){
-              res.json(rows[0])
-            }else{
-              res.json(rows)
-            }
-          }else{
-            console.log(`Dao Error:${error}`)
-            res.json({
-              "message":'error',
-              'table': `${table}`,
-              'error': error
-            })
+  findAll: (res, table) => {
+    connect.query(
+      `SELECT * FROM ${table};`,
+      (error, rows) => {
+        if (!error) {
+          if (rows.length === 1) {
+            res.json(rows[0]);
+          } else {
+            res.json(rows);
           }
-        }                  
-      )
-    }
-}
+        } else {
+          console.log(`Dao Error:${error}`);
+          res.json({
+            message: 'error',
+            table: `${table}`,
+            error: error
+          });
+        }
+      }
+    );
+  }
+};
 
-module.exports.daoCommon
+module.exports = daoCommon;
