5de16c9f8a05240bcc887fa6d3f4313a8c924524
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-11-26 17:53:35 -0600

    added images added nav bar

--- routes/api/programsRoutes.js
+++ routes/api/programsRoutes.js
@@ -10,41 +10,41 @@ router.get('/', (req, res)=> {
     dao.findAll(res, dao.table)            
 }) 
 
-// http://localhost:3000/api/programs/with_actors
-// router.get('/with_actors', (req, res)=> {
-//     dao.findProgramsWithActors(res, dao.table)
-// })
-// // http://localhost:3000/api/movie/with_genres
-// router.get('/with_genres', (req, res)=> {
-//     dao.findProgramsWithGenres(res, dao.table)
-// })
-// //http://localhost:3000/api/movie/get_movieDirector/?
-// router.get('/get_programsDirector/:id',(req, res)=> {
-//     dao.findProgramsByDirectorId(res, dao.table, req.params.id)
-// })
+http://localhost:3000/api/programs/with_actors
+router.get('/with_actors', (req, res)=> {
+    dao.findProgramsWithActors(res, dao.table)
+})
+// http://localhost:3000/api/movie/with_genres
+router.get('/with_genres', (req, res)=> {
+    dao.findProgramsWithGenres(res, dao.table)
+})
+//http://localhost:3000/api/movie/get_movieDirector/?
+router.get('/get_programsDirector/:id',(req, res)=> {
+    dao.findProgramsByDirectorId(res, dao.table, req.params.id)
+})
 
-// router.get('/get_programsActor/:id', (req, res)=>{
-//     dao.findProgramsByActorId(res, dao.table, req.params.id)
-// })
-// router.get('/get_movieProduction/:id',(req, res)=> {
-//     dao.FindProgramsProduction(res, dao.table)
-// })                                       
-// router.get('/sort/:sorter', (req, res)=> {    
-//     dao.sort(res, dao.table, req.params.sorter)
-// })                                  
-// //http://localhost:3000/api/:id
-// router.get('/:id', (req, res)=> {               
-//     dao.findById(res, dao.table, req.params.id)          
-// })  
-// //POST
-// router.post('/create',(req, res)=> {
-//   dao.create(req, res, dao.table)
-// })
+router.get('/get_programsActor/:id', (req, res)=>{
+    dao.findProgramsByActorId(res, dao.table, req.params.id)
+})
+router.get('/get_movieProduction/:id',(req, res)=> {
+    dao.FindProgramsProduction(res, dao.table)
+})                                       
+router.get('/sort/:sorter', (req, res)=> {    
+    dao.sort(res, dao.table, req.params.sorter)
+})                                  
+//http://localhost:3000/api/:id
+router.get('/:id', (req, res)=> {               
+    dao.findById(res, dao.table, req.params.id)          
+})  
+//POST
+router.post('/create',(req, res)=> {
+  dao.create(req, res, dao.table)
+})
 
-// //PATCH
-// router.patch('/update/:id', (req, res)=> {
-//   dao.update(req, res,dao.table)
-// })
+//PATCH
+router.patch('/update/:id', (req, res)=> {
+  dao.update(req, res,dao.table)
+})
                                                 
 
 module.exports = router                         
