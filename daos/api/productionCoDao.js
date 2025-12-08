06a07a36dff5eaad53244f3b565b78ec2381e87f
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-12-08 13:57:35 -0600

    not broken saving

--- daos/api/productionCoDao.js
+++ daos/api/productionCoDao.js
@@ -3,7 +3,6 @@ const connect = require('../../config/dbconfig')
 
 const productionCoDao = {
     table: 'productionCo',
-  
        //3 byPrograms
     findProgramsByProductionCo(res, table, id) {
         let sql = `SELECT
@@ -31,17 +30,12 @@ const productionCoDao = {
             }
         });
     },
-    // 5. unique1
-    findfivePointRatingByProductionCo(res, table) {
-        let sql = `SELECT 
-            pc.productionCo_id,
-            pc.productionCo,
-            p.fivePointRating AS programs_fivePointRating,
-            p.title AS programs_title
-            FROM productionCo AS pc
-            INNER JOIN programs AS p ON pc.productionCo_id = p.productionCo_id
-            ORDER BY fivePointRating DESC;`
-        connect.execute(
+    //alhabetized
+    productionCoAbc(res, table) {
+        let sql = `SELECT genre, genre_id
+            FROM genre 
+            ORDER BY genre ASC;`;
+        connect.query(
             sql,
             (error, rows) => {
                 if (!error) {
@@ -60,6 +54,37 @@ const productionCoDao = {
                 }
             }
         );
-    }
+    },
+        // ...existing code...
+        // 5. unique1
+        findfivePointRatingByProductionCo(res, table) {
+            let sql = `SELECT 
+                pc.productionCo_id,
+                pc.productionCo,
+                p.fivePointRating AS programs_fivePointRating,
+                p.title AS programs_title
+                FROM productionCo AS pc
+                INNER JOIN programs AS p ON pc.productionCo_id = p.productionCo_id
+                ORDER BY fivePointRating DESC;`
+            connect.execute(
+                sql,
+                (error, rows) => {
+                    if (!error) {
+                        if (rows.length === 1) {
+                            res.json(...rows);
+                        } else {
+                            res.json(rows);
+                        }
+                    } else {
+                        console.log(`DAO Error: ${error}`);
+                        res.json({
+                            message: "error",
+                            table: `productionCo`,
+                            error: error,
+                        });
+                    }
+                }
+            );
+        }
 };
 module.exports = productionCoDao;
--- routes/api/productionCoRoutes.js
+++ routes/api/productionCoRoutes.js
@@ -2,7 +2,9 @@
 const router = require('express').Router()
 
 const {productionCoDao: dao} = require('../../daos/dao')
-
+router.get('/get_productionCoAbc/:id', (req, res)=> {
+  dao.productionCoAbc(res, dao.table, req.params.id)
+})
 //3. ByPrograms http://localhost:3000/api/productionCo/get_programsForProductionCo/?
 router.get('/get_programsByProductionCo/:id', (req, res)=> {
   dao.findProgramsByProductionCo(res, dao.table, req.params.id)
